1be87ef36056c2ce3be383ef7ffb7c4f2c0344f1
SengSetha <sethaseng2@gmail.com>
2026-01-06 12:10:48 +0700

    implement code inside User,java

--- TestManagementSystem/src/auth/User.java
+++ TestManagementSystem/src/auth/User.java
@@ -1,5 +1,31 @@
 package auth;
 
-public class User {
-    
+public abstract class User {
+    protected int userId;
+    protected String name;
+    protected String email;
+    protected String password;
+    protected Role role;
+
+    // Constructor
+    public User(int userId, String name, String email, String password, Role role) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Common methods
+    public void login() {
+        System.out.println(name + " logged in as " + role);
+    }
+
+    public void logout() {
+        System.out.println(name + " logged out.");
+    }
+
+    // Abstract method - subclasses must implement
+    public abstract void displayInfo();
 }
+
