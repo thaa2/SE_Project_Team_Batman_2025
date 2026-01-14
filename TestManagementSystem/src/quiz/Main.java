5267e5222e2af16dc57e3780408b240579ddd4a9
Rosany Mat <rosanymat13@gmail.com>
2026-01-14 23:09:43 +0700

    new version

--- TestManagementSystem/src/quiz/Main.java
+++ TestManagementSystem/src/quiz/Main.java
@@ -1,4 +1,4 @@
-package quiz;
+package SE_project;
 
 import java.util.Scanner;
 
@@ -17,10 +17,11 @@ public class Main {
                 System.out.println("\n=== MAIN MENU ===");
                 System.out.println("1. Add New Questions");
                 System.out.println("2. Show All Questions");
-                System.out.println("3. Attempt Quiz");
-                System.out.println("4. View All Students Results");
+                System.out.println("3. Delete a Question");
+                System.out.println("4. Attempt Quiz");
+                System.out.println("5. View All Students Results");
                 System.out.println("0. Exit");
-                System.out.print("Choose an option (0-4): ");
+                System.out.print("Choose an option (0-5): ");
                 
                 String input = sc.nextLine();
                 
@@ -30,23 +31,24 @@ public class Main {
                     switch (choice) {
                         case 1 -> quizManager.addQuestions(sc);
                         case 2 -> service.printAllQuestions();
-                        case 3 -> quizManager.attemptQuiz(sc);
-                        case 4 -> quizManager.viewAllStudentsResults(sc);
+                        case 3 -> quizManager.deleteQuestionOperation(sc);
+                        case 4 -> quizManager.attemptQuiz(sc);
+                        case 5 -> quizManager.viewAllStudentsResults(sc);
                         case 0 -> {
                             System.out.println("Goodbye!");
                             sc.close();
                             return;
                         }
-                        default -> System.out.println(" Invalid choice! Please select 0-4.");
+                        default -> System.out.println(" Invalid choice! Please select 0-5.");
                     }
                 } catch (NumberFormatException e) {
-                    System.out.println(" Please enter a valid number (0-4)!");
+                    System.out.println(" Please enter a valid number (0-5)!");
                 }
             }
         } catch (Exception e) {
             System.err.println(" Fatal error: " + e.getMessage());
+            e.printStackTrace();
             sc.close();
         }
     }
 }
-
